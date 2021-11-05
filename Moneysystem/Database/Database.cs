9774c205befc52bf4c01f74268abdbd93e531324
myrlejon <60348075+myrlejon@users.noreply.github.com>
2021-11-05 15:35:09 +0100

    Added some structure.

---
+++ Moneysystem/Database/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moneysystem.Models;
+
+namespace Moneysystem.Database
+{
+    public class Database : DbContext
+    {
+        private const string DatabaseName = "Moneysystem";
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Book> Books { get; set; }
+        //public DbSet<BookCategory> BookCategory { get; set; }
+        //public DbSet<SoldBooks> SoldBooks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($@"Server=.\SQLExpress;Database={DatabaseName}; Trusted_Connection = true;");
+        }
+    }
+}
