cbe08ef667bfadeb8ad088b6dcee6cb6bf2b6aa4
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-11-12 16:45:49 +0100

    Stored the TestSuite properly outside the Moneysystem project folder v2

---
+++ TestSuite/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSuite
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
