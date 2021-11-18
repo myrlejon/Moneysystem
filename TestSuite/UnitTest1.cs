e287917bec42c16a0fa15b3adf18103627aa0929
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-11-18 14:20:40 +0100

    Added a regex-expression

--- TestSuite/UnitTest1.cs
+++ TestSuite/UnitTest1.cs
@@ -1,15 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moneysystem.Utilities;
 using System;
+using System.Text.RegularExpressions;
 
 namespace TestSuite
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestPassword()
+        [DataRow("")]
+        [DataRow("t123abdkq")]
+        [DataRow("t123abdkq!")]
+        [DataTestMethod]
+        public void TestPassword(string value)
         {
-            Assert.IsTrue(Moneysystem.Utilities.PasswordChecker.CheckPassword("t123abdkq"));
+            Regex regexItem = new Regex("^[a-zA-Z0-9]+$");
+            bool result = regexItem.IsMatch(value);
+            if (result)
+            {
+                Assert.IsTrue(PasswordChecker.CheckPassword(value));
+            }
+            else
+            {
+                Assert.IsFalse(PasswordChecker.CheckPassword(value));
+            }
         }
     }
 }
